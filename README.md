cbf40b6313ebe0f0fea25e8b2ad107e92fe2267e
Jose Segura <jossegura2023@u.northwestern.edu>
2023-03-07 17:30:00 -0600

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # SunnySide-Weather-Dashboard
+
+## Description 
+This app is supposed to provide a weather dashboard that provides current day weather data and the following 4 days. The user can input a city
+and the app would provide the weather for the city for that current day. It's supposed to be simple to use, and easy to read, the previous results would also display under the search bar and saved to local storage. Using the OpenWeather API, we use the data to to retrieve information regarding the user input.
+
+## Usage 
+This apps intentions is to provide a dynamic 5 day forecast that would pull the information of a city the user inputs. This would display
+the temperature, humidity, wind speed, and description. This would allow users to interact with a weather dashboard that is simple and easy to understand. 
+
+## Deployment
+
+## Github Repo
+https://github.com/Jose-seg/SunnySide-Weather-Dashboard
--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,22 @@
-// Get a reference to the previous-results container
-const previousResultsContainer = document.getElementById('previous-results');
+const apiKey = '44dda8f699c5f405c52085f983a2038d'; // Replace with your own API key
+const city = 'London'; // Replace with the city name you want to search for
 
-// Create a new div element for the search result
-const searchResultDiv = document.createElement('div');
+// Build the API endpoint URL
+const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`;
 
-// Set the content of the search result div
-searchResultDiv.textContent = 'Your search result goes here';
-
-// Append the search result div to the previous-results container
-previousResultsContainer.appendChild(searchResultDiv);
+// Make a fetch request to the API endpoint
+fetch(geoUrl)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Unable to retrieve location data');
+    }
+  })
+  .then(data => {
+    // Log the latitude and longitude coordinates of the first result
+    console.log(`Latitude: ${data[0].lat}, Longitude: ${data[0].lon}`);
+  })
+  .catch(error => {
+    console.error(error);
+  });
