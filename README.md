e4eb3f2fc90bd85c0f89349b1c73cda552b6f354
Jose Segura <jossegura2023@u.northwestern.edu>
2023-03-27 14:15:56 -0500

    added comments to code and screenshots to readme

--- README.md
+++ README.md
@@ -8,6 +8,11 @@ and the app would provide the weather for the city for that current day. It's su
 This apps intentions is to provide a dynamic 5 day forecast that would pull the information of a city the user inputs. This would display
 the temperature, humidity, wind speed, and description. This would allow users to interact with a weather dashboard that is simple and easy to understand. 
 
+## Screenshot
+![image](https://user-images.githubusercontent.com/122575280/228043001-f60f9bac-cae2-4718-91cb-23cedfe3b49e.png)
+![image](https://user-images.githubusercontent.com/122575280/228043157-8fc14bcd-fcde-4a33-aad3-3c7fad82efab.png)
+![image](https://user-images.githubusercontent.com/122575280/228043314-5a608bf3-7f3c-4fe0-89bc-f85139a74048.png)
+
 ## Deployment
 https://jose-seg.github.io/SunnySide-Weather-Dashboard/
 ## Github Repo
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-
+//const variables for the elements will interact with in html
 const API_KEY = '44dda8f699c5f405c52085f983a2038d';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
 
@@ -8,29 +8,34 @@ const currentWeatherDiv = document.getElementById('current-weather');
 const forecastDiv = document.getElementById('forecast');
 const searchHistoryList = document.getElementById('search-history');
 
+//button functionality for the search section
 searchForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+    e.preventDefault(); // prevents page from refreshing
     const city = cityInput.value;
     fetchWeather(city);
     //Clears search bar
     cityInput.value = '';
 });
 
+//Function to get current day weather
 function fetchWeather(city) {
     // Fetch current weather
     const currentWeatherUrl = `${BASE_URL}weather?q=${city}&appid=${API_KEY}&units=metric`;
     fetch(currentWeatherUrl)
         .then((response) => response.json())
         .then((data) => {
+            //I console logged to see if i was getting any data back
             console.log('current weather data', data);
             displayCurrentWeather(data);
         });
     
-    // Fetch 5-day forecast
+    // url Fetch for 5-day forecast
     const forecastUrl = `${BASE_URL}forecast?q=${city}&appid=${API_KEY}&units=metric`;
+    // Fetching the forecast for the next four days
     fetch(forecastUrl)
         .then((response) => response.json())
     .then((data) => {
+        //I console logged to see if i was getting any data back
         console.log('forecast data', data);
         displayForecast(data);
     });
@@ -40,13 +45,14 @@ function fetchWeather(city) {
 function displayCurrentWeather(data) {
     const {name, main, wind, weather} = data;
     const iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}.png`;
-
+//This will dispay the necessary information for the current day weather section
+// I used template literals to grab necessary specific info without writing long lines of code
     currentWeatherDiv.innerHTML = `
-    <h3>${name} (${new Date().toLocaleDateString()}) <img src='${iconUrl}' alt='weather icon'></h3>
+    <h3>${name} (${new Date().toLocaleDateString()}) <img src='${iconUrl}' alt='weather icon'></h3> 
     <p>Temperature: ${main.temp}°C</p>
     <p>Humidity: ${main.humidity}%</p>
     <p>Wind Speed: ${wind.speed} m/s</p>`;
-
+// Adds city name to search history
     addToSearchHistory(name);
 }
 
@@ -54,18 +60,18 @@ function displayCurrentWeather(data) {
 function displayForecast(data) {
     const {list} = data;
     const dailyData = [];
-
+//loops through the data we retrieve to get the forecast for the next four days
     for(let i = 0; i < list.length; i += 8) {
         dailyData.push(list[i]);
     }
     dailyData.shift(); //Takes away current day data
-
+// this modifies the html to insert the data and text we need for our app
     forecastDiv.innerHTML = dailyData
     .map((dayData) => {
         const {dt, main, wind, weather} = dayData;
         const iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}.png`;
-        const date = new Date(dt * 1000).toLocaleDateString();
-
+        const date = new Date(dt * 1000).toLocaleDateString();//  const to get our dates
+// inserts the html we created into the forecast day with our data
         return `<div class="forecast-day">
                     <h4>${date} <img src="${iconUrl}" alt="weather icon"></h4>
                     <p>Temperature: ${main.temp}°C</p>
@@ -84,7 +90,7 @@ function addToSearchHistory(city) {
         searchHistory.splice(5);
         localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
     }
-    loadSearchHistory();
+    loadSearchHistory(); // search history will be able to get called back and clicked to retrieve previously viewed data
 }
 
 // Load search history from local storage
@@ -102,4 +108,4 @@ function loadSearchHistory() {
     })
 }
 
-loadSearchHistory();
+loadSearchHistory(); // search history will be able to get called back and clicked to retrieve previously viewed data
